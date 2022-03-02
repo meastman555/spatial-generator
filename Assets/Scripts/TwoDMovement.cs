4d3e20e38cca134fd2397ca1bec9a95cec8057d2
Mason Eastman <masoneastman555@gmail.com>
2022-03-02 00:33:37 -0600

    initial commit, prefabed all possible rooms (tiles)

---
+++ Assets/Scripts/TwoDMovement.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class TwoDMovement : MonoBehaviour
+{
+
+    [SerializeField] float speed = 10;
+
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        HandleMovement();
+    }
+
+    //Checks input and handles movement
+    void HandleMovement(){
+        //moving in all 4 directions
+        if(Input.GetKey(KeyCode.W)){
+            rb.velocity = Vector2.up * speed;
+
+        }
+        else if(Input.GetKey(KeyCode.S)){
+            rb.velocity = Vector2.down * speed;
+
+        }
+        else if(Input.GetKey(KeyCode.A)){
+            rb.velocity = Vector2.left * speed;
+
+        }
+        else if(Input.GetKey(KeyCode.D)){
+            rb.velocity = Vector2.right * speed;
+
+        }
+        //stationary
+        else {
+            rb.velocity = Vector2.zero;
+        }
+    }
+}
